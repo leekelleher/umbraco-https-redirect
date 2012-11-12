2055c2953b8281d660f9ce5142cc7ba033c09b3f
vertino <none@none>
2012-11-12 14:29:37 +0000

    Bumped version number for Package build.

--- Our.Umbraco.HttpsRedirect/Properties/AssemblyInfo.cs
+++ Our.Umbraco.HttpsRedirect/Properties/AssemblyInfo.cs
@@ -1,26 +1,14 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("Our.Umbraco.HttpsRedirect")]
 [assembly: AssemblyDescription("HTTPS Redirect for Umbraco")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Umbrella Inc Ltd")]
 [assembly: AssemblyProduct("Our.Umbraco.HttpsRedirect")]
-[assembly: AssemblyCopyright("Copyright \xa9 Umbrella Inc Ltd 2011")]
+[assembly: AssemblyCopyright("Copyright \xa9 Umbrella Inc Ltd 2011-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible to COM components.
-// If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("066DE8E5-30D8-4911-A53D-2F21A24ADD4D")]
-
-// Version information for an assembly consists of the following four values:
-// [Major].[Minor].[Build].[Revision]
-[assembly: AssemblyVersion("1.1.1.*")]
-// [assembly: AssemblyFileVersion("1.1.0.*")]
