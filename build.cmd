d06549c093dff098dc98c40996f41a0f50e055ba
vertino <none@none>
2013-09-02 13:20:16 +0100

    Fixed the MSBuild script for creating the Umb & NuGet packages.
    --HG--
    branch : feature/nuget

--- build.cmd
+++ build.cmd
@@ -1,2 +1,7 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe BuildPackage.msbuild
-PAUSE
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe package\package.proj
+
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@EXIT /B 0
+:err
+@PAUSE
+@EXIT /B 1
