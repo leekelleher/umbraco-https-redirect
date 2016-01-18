9643cffb83050008284d8715665cf1ffd525f98c
leekelleher <leekelleher@gmail.com>
2016-01-18 17:04:19 +0000

    Updated NuGet.exe and batch file.
    Also fixed an assembly reference for Umbraco.Core.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe package\package.proj
+REM CALL src\.nuget\NuGet.exe restore src\Our.Umbraco.HttpsRedirect.sln
+CALL "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" package\package.proj
 
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @EXIT /B 0
